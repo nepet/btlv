28a2801579005da257e87c049b0781082b4e9162
Peter Neuroth <pet.v.ne@gmail.com>
2025-09-11 17:26:39 +0200

    scaffold cli
    Signed-off-by: Peter Neuroth <pet.v.ne@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::{Parser, Subcommand};
+
+/// Main args of tlv-tool
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Tool {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum Commands {
+    /// Encodes TLV-stream into hex.
+    Encode {
+        /// Json encoded data to encode into hex TLV-stream.
+        data: String,
+    },
+    /// Decodes TLV-stream from hex.
+    Decode {
+        /// Hex-encoded String.
+        data: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let tool = Tool::parse();
+
+    match tool.command {
+        Commands::Encode { data } => println!("encode {data}"),
+        Commands::Decode { data } => println!("decode {data}"),
+    }
 }
